5261141a8e0c76c5cbd65399719413e04bc64d84
Subin Silwal <Subin.Silwal@bankerstoolbox.com>
2017-12-04 09:51:03 -0600

    Refactor
    Refactor

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -22,24 +22,5 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 @import "../../node_modules/bulma/css/bulma.css";
-/*
-h1, h2 {
-  font-weight: normal;
-  font-family: "Times New Roman", Times, serif;
-}
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
-*/
 </style>
