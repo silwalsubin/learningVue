05757002cdf40c67b5f54315932682f2e1cce303
Subin Silwal <Subin.Silwal@bankerstoolbox.com>
2017-12-21 11:22:05 -0600

    little progress google auth
    little progress google auth

--- src/components/platform.js
+++ src/components/platform.js
@@ -1,3 +1,9 @@
+
+
+// gapi.auth2.init({
+//   client_id: `817728938926-ctui6aa8ri3ge1tjltd396h8qtars40s.apps.googleusercontent.com`
+// })
+
 function gapiLoadedPromise() {
   return new Promise((resolve, reject) => {
     console.log(`Gapi type:  ${typeof gapi}`);
--- src/components/sign-in-button.vue
+++ src/components/sign-in-button.vue
@@ -1,50 +1,83 @@
 <template>
   <div>
-    <button @click="getInfo()">Click Me</button>
-    <div class="g-signin2" data-onsuccess="onSignIn"></div>
+    <button @click="signOut()">Sign out</button>
+    <button @click="getProfileInfo()">ProfilInfo</button>
+    <button id="signin-button" ref="signinBtn">Sign In </button>
+    <div class="g-signin2" googleUser="onSignin()"></div>
   </div>
 </template>
 
-
-<!-- <script src="https://apis.google.com/js/platform.js?onload=renderButton" async defer></script> -->
-
-
-
 <script >
   import googleApi from './platform'
-
-  function onSuccess(googleUser) {
-    console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
-  }
-  function onFailure(error) {
-    console.log(error);
-    console.log("asdfasdf");
-  }
-
-  function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-  };
+  import googlePlatform from './googlePlatform'
 
   export default {
     name: 'sign-in-button',
+    props: {
+      isUserSignedin : {
+        type: Boolean,
+        default: false
+      }
+    },
     data(){
       return {
-        url: "https://apis.google.com/js/platform.js"
+        url: "https://apis.google.com/js/platform.js",
+        user:{}
       }
     },
     methods: {
+      appStart(){
+        gapi.load(this.user, this.initClient());
+      },
+      initClient(){
+        console.log("is there anybody out there? ")
+          this.user = gapi.auth2.init({
+            client_id: `817728938926-ctui6aa8ri3ge1tjltd396h8qtars40s.apps.googleusercontent.com`,
+            scope: 'profile'
+          });
+      },
+      onSignin(){
+        console.log("function called");
+      },
+      signOut(){
+        this.user = gapi.auth2.getAuthInstance();
+        if (this.user.isSignedIn.get()){
+          this.user.signOut().then(function () {
+            console.log('User signed out.');
+          });
+        }
+        else{
+          console.log('User already signed out.');
+        }
+      },
+      getProfileInfo(){
+        let userInstance = gapi.auth2.getAuthInstance();
+        console.log(userInstance.isSignedIn.get());
 
+        if (userInstance.isSignedIn.get()) {
+          var profile = userInstance.currentUser.get().getBasicProfile();
+          console.log('ID: ' + profile.getId());
+          console.log('Full Name: ' + profile.getName());
+          console.log('Given Name: ' + profile.getGivenName());
+          console.log('Family Name: ' + profile.getFamilyName());
+          console.log('Image URL: ' + profile.getImageUrl());
+          console.log('Email: ' + profile.getEmail());
+        }
+      }
     },
     mounted() {
+
+
       googleApi.gapiLoadedPromise().then(() => {
-        console.log("in the dom")
+        console.log("in the dom");
       }).catch(() => {
         console.log("its not in the  dom")
       });
+    },
+    computed: {
+      // isUserSignedin(){
+      //   return gapi.auth2.getAuthInstance().isSignedIn.get();
+      // }
     }
   }
 </script>
